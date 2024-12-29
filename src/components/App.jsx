8fcbe0fcf185c8989d23146bf6efadd4a4f1c656
AlexKaplan <alexkaplan110606@gmail.com>
2024-12-29 18:52:31 +0200

    3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,22 @@
-// src/App.jsx
+// src/components/App.jsx
 
-import Product from './Product';
+import Product from "./Product";
 
 export default function App() {
   return (
     <div>
       <h1>Best selling</h1>
 
-      <Product />
-      <Product />
-      <Product />
+      <Product
+        name="Tacos With Lime"
+        img="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+        price={10.99}
+      />
+      <Product
+        name="Fries and Burger"
+        img="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+        price={14.29}
+      />
     </div>
   );
 }
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,9 +1,7 @@
-export default function Product() {
-    const price = 999;
-    const img = "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640";
+export default function Product({ name, img, price }) {
     return (
         <div>
-            <h2>Tacos</h2>
+            <h2>{name}</h2>
             <img src={img} alt="Tacos With Lime" width="640" />
             <p>Price: {price} credits</p>
         </div>
