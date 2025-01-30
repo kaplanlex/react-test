cf6ebf0b193a469d09f6f948f524dfa12c1c69f8
AlexKaplan <alexkaplan110606@gmail.com>
2025-01-30 16:44:49 +0200

    8

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -43,45 +43,40 @@ export default function App() {
 
 {/*// src/components/App.jsx
 
-import { Routes, Route, NavLink } from "react-router-dom";
-import clsx from 'clsx';
+import { Routes, Route, Link } from "react-router-dom";
 import Home from "path/to/pages/Home";
 import About from "path/to/pages/About";
 import Products from "path/to/pages/Products";
-import NotFound from 'path/to/pages/NotFound';
-import css from './App.module.css';
-
-const buildLinkClass = ({ isActive }) => {
-  return clsx(css.link, isActive && css.active);
-};
+import NotFound from "path/to/pages/NotFound";
+import ProductDetails from "path/to/pages/ProductDetails";
+import Mission from "path/to/components/Mission";
+import Team from "path/to/components/Team";
+import Reviews from "path/to/components/Reviews";
 
 export const App = () => {
-  return (
+  return (
     <div>
-      <nav className={css.nav}>
-        <NavLink to="/" className={buildLinkClass}>
-          Home
-        </NavLink>
-        <NavLink to="/about" className={buildLinkClass}>
-          About
-        </NavLink>
-        <NavLink to="/products" className={buildLinkClass}>
-          Products
-        </NavLink>
-      </nav>
-  
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/products" element={<Products />} />
-  		<Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/products">Products</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />}>
+          <Route path="mission" element={<Mission />} />
+          <Route path="team" element={<Team />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:productId" element={<ProductDetails />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
   );
 };
-
-
- */}
+*/
+}
 
 
 {/*const App = () => {
